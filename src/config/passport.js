5af69e4f4fc99ec003e78aab07da4d3580a87877
suyash-voosh <suyash@voosh.in>
2025-10-26 03:17:37 +0530

    minor changes

--- src/config/passport.js
+++ src/config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.BACKEND_URL 
+      callbackURL: process.env.BACKEND_URL
         ? `${process.env.BACKEND_URL}/api/auth/google/callback`
         : '/api/auth/google/callback',
     },
